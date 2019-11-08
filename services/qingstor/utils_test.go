14174cc838517d126d14210f8a3a4e54be1fe751
Xuanwo <github@xuanwo.io>
2019-11-08 11:09:49 +0800

    services/qingstor: Add tests for handleQingStorError

--- services/qingstor/utils_test.go
+++ services/qingstor/utils_test.go
@@ -1,7 +1,12 @@
 package qingstor
 
 import (
+	"errors"
 	"testing"
+
+	"github.com/Xuanwo/storage/types"
+	"github.com/stretchr/testify/assert"
+	qserror "github.com/yunify/qingstor-sdk-go/v3/request/errors"
 )
 
 func TestIsBucketNameValid(t *testing.T) {
@@ -24,5 +29,114 @@ func TestIsBucketNameValid(t *testing.T) {
 				t.Errorf("IsBucketNameValid() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func TestHandleQingStorError(t *testing.T) {
+	t.Run("nil error will panic", func(t *testing.T) {
+		assert.Panics(t, func() {
+			_ = handleQingStorError(nil)
+		})
+	})
+
+	t.Run("non-qingstor error will return a ErrUnhandledError", func(t *testing.T) {
+		err := handleQingStorError(errors.New("test"))
+		assert.True(t, errors.Is(err, types.ErrUnhandledError))
+	})
+
+	{
+		tests := []struct {
+			name     string
+			input    *qserror.QingStorError
+			expected error
+		}{
+			{
+				"not found",
+				&qserror.QingStorError{
+					StatusCode:   404,
+					Code:         "",
+					Message:      "",
+					RequestID:    "",
+					ReferenceURL: "",
+				},
+				types.ErrObjectNotExist,
+			},
+			{
+				"invalid status code",
+				&qserror.QingStorError{
+					StatusCode:   444,
+					Code:         "",
+					Message:      "",
+					RequestID:    "",
+					ReferenceURL: "",
+				},
+				types.ErrUnhandledError,
+			},
+		}
+
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				assert.True(t, errors.Is(handleQingStorError(tt.input), tt.expected))
+			})
+		}
+	}
+
+	{
+		tests := []struct {
+			name     string
+			input    *qserror.QingStorError
+			expected error
+		}{
+			{
+				"permission_denied",
+				&qserror.QingStorError{
+					StatusCode:   403,
+					Code:         "permission_denied",
+					Message:      "",
+					RequestID:    "",
+					ReferenceURL: "",
+				},
+				types.ErrPermissionDenied,
+			},
+			{
+				"object_not_exists",
+				&qserror.QingStorError{
+					StatusCode:   404,
+					Code:         "object_not_exists",
+					Message:      "",
+					RequestID:    "",
+					ReferenceURL: "",
+				},
+				types.ErrObjectNotExist,
+			},
+			{
+				"invalid_access_key_id",
+				&qserror.QingStorError{
+					StatusCode:   400,
+					Code:         "invalid_access_key_id",
+					Message:      "",
+					RequestID:    "",
+					ReferenceURL: "",
+				},
+				types.ErrConfigIncorrect,
+			},
+			{
+				"not handled",
+				&qserror.QingStorError{
+					StatusCode:   400,
+					Code:         "xxxxxx",
+					Message:      "",
+					RequestID:    "",
+					ReferenceURL: "",
+				},
+				types.ErrUnhandledError,
+			},
+		}
+
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				assert.True(t, errors.Is(handleQingStorError(tt.input), tt.expected))
+			})
+		}
 	}
 }
