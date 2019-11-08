9d40af20410816c80fb9bacb67f83446fc1782d1
Prnyself <281239768@qq.com>
2019-11-08 15:42:13 +0800

    services/posixfs: Add tests for handleOsError, move utils into separated file.

--- services/posixfs/errors.go
+++ services/posixfs/errors.go
@@ -1,19 +1,1 @@
 package posixfs
-
-import (
-	"os"
-
-	"github.com/Xuanwo/storage/types"
-)
-
-func handleOsError(err error) error {
-	if err == nil {
-		panic("error must not be nil")
-	}
-
-	if os.IsNotExist(err) {
-		return types.ErrObjectNotExist
-	}
-	// TODO: handle other osError here.
-	return err
-}
