bd7682934573be3af0d39c91acfe0c655c18e9bb
Xuanwo <github@xuanwo.io>
2019-11-18 11:10:13 +0800

    types: Refactor pairs and metadata

---
+++ types/metadata/types.go
@@ -0,0 +1,6 @@
+package metadata
+
+// Metadata will hold storager or object's metadata.
+//
+//go:generate ../../internal/bin/metadata
+type Metadata map[string]interface{}
--- types/struct.go
+++ types/struct.go
@@ -1,5 +1,9 @@
 package types
 
+import (
+	"github.com/Xuanwo/storage/types/metadata"
+)
+
 // ServicerType is the type for service, under layer type is string.
 type ServicerType string
 
@@ -25,39 +29,14 @@ type Object struct {
 	Type ObjectType
 
 	// metadata is the metadata of the object.
-	Metadata
+	metadata.Metadata
 }
 
-// Metadata is the metadata used in object.
-type Metadata map[string]interface{}
+// ObjectFunc will handle an Object.
+type ObjectFunc func(object *Object)
 
 // Pair will store option for storage service.
-//
-//go:generate ../internal/bin/pairs
 type Pair struct {
 	Key   string
 	Value interface{}
-}
-
-// AvailablePairs are all available options for storage.
-// This will be used to generate options.go
-var AvailablePairs = map[string]string{
-	"access_key":    "string",
-	"checksum":      "string",
-	"count":         "int64",
-	"expire":        "int",
-	"host":          "string",
-	"location":      "string",
-	"name":          "string",
-	"offset":        "int64",
-	"port":          "int",
-	"protocol":      "string",
-	"recursive":     "bool",
-	"secret_key":    "string",
-	"size":          "int64",
-	"storage_class": "string",
-	"type":          "string",
-	"updated_at":    "time.Time",
-	"work_dir":      "string",
-	"part_size":     "int64",
 }
