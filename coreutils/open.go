76595e3dc848c17b0dfa59a6c485b6389652274d
Xuanwo <github@xuanwo.io>
2019-12-23 15:01:50 +0800

    coreutils: Add fs support

--- coreutils/open.go
+++ coreutils/open.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/Xuanwo/storage"
 	"github.com/Xuanwo/storage/pkg/config"
-	"github.com/Xuanwo/storage/services/posixfs"
+	"github.com/Xuanwo/storage/services/fs"
 	"github.com/Xuanwo/storage/services/qingstor"
 	"github.com/Xuanwo/storage/types/pairs"
 )
@@ -19,7 +19,7 @@ var (
 // Open will parse config string and return valid Servicer and Storager.
 //
 // Depends on config string's content, Storager could be nil if namespace not given.
-func Open(cfg string) (srv storage.Servicer, stor storage.Storager, err error) {
+func Open(cfg string) (srv storage.Servicer, store storage.Storager, err error) {
 	errorMessage := "coreutils Open [%s]: <%w>"
 
 	t, namespace, opt, err := config.Parse(cfg)
@@ -28,7 +28,14 @@ func Open(cfg string) (srv storage.Servicer, stor storage.Storager, err error) {
 	}
 
 	switch t {
-	case posixfs.Type:
+	case fs.Type:
+		store = fs.New()
+		path := fs.ParseNamespace(namespace)
+		err = store.Init(pairs.WithWorkDir(path))
+		if err != nil {
+			err = fmt.Errorf(errorMessage, cfg, err)
+			return
+		}
 		return
 	case qingstor.Type:
 		srv, err = qingstor.New(opt...)
@@ -40,12 +47,12 @@ func Open(cfg string) (srv storage.Servicer, stor storage.Storager, err error) {
 		if name == "" {
 			return
 		}
-		stor, err = srv.Get(name)
+		store, err = srv.Get(name)
 		if err != nil {
 			err = fmt.Errorf(errorMessage, cfg, err)
 			return
 		}
-		err = stor.Init(pairs.WithWorkDir(prefix))
+		err = store.Init(pairs.WithWorkDir(prefix))
 		if err != nil {
 			err = fmt.Errorf(errorMessage, cfg, err)
 			return
