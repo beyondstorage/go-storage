c8eb9742f4e100f23ddf8cfce1c697e18cd97104
Xuanwo <github@xuanwo.io>
2020-06-29 17:07:10 +0800

    services/fs: Convert system specific separator to slash
    Signed-off-by: Xuanwo <github@xuanwo.io>

---
+++ services/fs/storage_windows_test.go
@@ -0,0 +1,63 @@
+package fs
+
+import (
+	"fmt"
+)
+
+func TestStorage_ListDirUnderWindows(t *testing.T) {
+	paths := make([]string, 100)
+	for k := range paths {
+		paths[k] = uuid.New().String()
+	}
+
+	tests := []struct {
+		name  string
+		fi    []os.FileInfo
+		items []*types.Object
+		err   error
+	}{
+		{
+			"success file recursively",
+			[]os.FileInfo{
+				fileInfo{
+					name:    "test_file",
+					size:    1234,
+					mode:    0644,
+					modTime: time.Unix(1, 0),
+				},
+			},
+			[]*types.Object{
+				{
+					ID:         filepath.Join(paths[1], "test_file"),
+					Name:       fmt.Sprintf("%s/%s", paths[1], "test_file"),
+					Type:       types.ObjectTypeFile,
+					Size:       1234,
+					UpdatedAt:  time.Unix(1, 0),
+					ObjectMeta: info.NewObjectMeta(),
+				},
+			},
+			nil,
+		},
+	}
+
+	for k, v := range tests {
+		t.Run(v.name, func(t *testing.T) {
+			client := &Storage{
+				ioutilReadDir: func(dirname string) (infos []os.FileInfo, e error) {
+					assert.Equal(t, paths[k], dirname)
+					return v.fi, v.err
+				},
+			}
+
+			items := make([]*types.Object, 0)
+
+			err := client.ListDir(paths[k], pairs.WithDirFunc(func(object *types.Object) {
+				items = append(items, object)
+			}), pairs.WithFileFunc(func(object *types.Object) {
+				items = append(items, object)
+			}))
+			assert.Equal(t, v.err == nil, err == nil)
+			assert.EqualValues(t, v.items, items)
+		})
+	}
+}
