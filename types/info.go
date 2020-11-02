ddf2864ceab97fecebd2633feed21d525434962f
Xuanwo <github@xuanwo.io>
2020-11-02 17:35:18 +0800

    types: Trim comments
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- types/info.go
+++ types/info.go
@@ -1,17 +1,11 @@
 package types
 
-// StorageMeta is the static metadata for StorageMeta.
-// type StorageMeta storageMeta
-
 // NewStorageMeta will create a new StorageMeta metadata.
 func NewStorageMeta() *StorageMeta {
 	return &StorageMeta{
 		m: make(map[string]interface{}),
 	}
 }
-
-// StorageStatistic is the statistic metadata for StorageMeta.
-// type StorageStatistic storageStatistic
 
 // NewStorageStatistic will create a new StorageMeta statistic.
 func NewStorageStatistic() *StorageStatistic {
