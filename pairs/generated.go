3eae715cf1a39f3ce73c134ac045466cb0542848
Xuanwo <github@xuanwo.io>
2020-11-04 13:21:50 +0800

    Fix typo
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- pairs/generated.go
+++ pairs/generated.go
@@ -10,154 +10,154 @@ import (
 	. "github.com/aos-dev/go-storage/v2/types"
 )
 
-// WithcontentMd5 will apply content_md5 value to Options
+// WithContentMd5 will apply content_md5 value to Options
 // ContentMd5
-func WithcontentMd5(v string) Pair {
+func WithContentMd5(v string) Pair {
 	return Pair{
 		Key:   "content_md5",
 		Value: v,
 	}
 }
 
-// WithcontentType will apply content_type value to Options
+// WithContentType will apply content_type value to Options
 // ContentType
-func WithcontentType(v string) Pair {
+func WithContentType(v string) Pair {
 	return Pair{
 		Key:   "content_type",
 		Value: v,
 	}
 }
 
-// Withcontext will apply context value to Options
+// WithContext will apply context value to Options
 // Context context in all request
-func Withcontext(v context.Context) Pair {
+func WithContext(v context.Context) Pair {
 	return Pair{
 		Key:   "context",
 		Value: v,
 	}
 }
 
-// WithcontinuationToken will apply continuation_token value to Options
+// WithContinuationToken will apply continuation_token value to Options
 // ContinuationToken specify the continuation token for list_dir or list_prefix.
-func WithcontinuationToken(v string) Pair {
+func WithContinuationToken(v string) Pair {
 	return Pair{
 		Key:   "continuation_token",
 		Value: v,
 	}
 }
 
-// Withcredential will apply credential value to Options
+// WithCredential will apply credential value to Options
 // Credential specify how to provide credential for service or storage
-func Withcredential(v *credential.Provider) Pair {
+func WithCredential(v *credential.Provider) Pair {
 	return Pair{
 		Key:   "credential",
 		Value: v,
 	}
 }
 
-// Withendpoint will apply endpoint value to Options
+// WithEndpoint will apply endpoint value to Options
 // Endpoint specify how to provide endpoint for service or storage
-func Withendpoint(v endpoint.Provider) Pair {
+func WithEndpoint(v endpoint.Provider) Pair {
 	return Pair{
 		Key:   "endpoint",
 		Value: v,
 	}
 }
 
-// Withexpire will apply expire value to Options
+// WithExpire will apply expire value to Options
 // Expire specify when the url returned by reach will expire
-func Withexpire(v int) Pair {
+func WithExpire(v int) Pair {
 	return Pair{
 		Key:   "expire",
 		Value: v,
 	}
 }
 
-// WithhttpClientOptions will apply http_client_options value to Options
+// WithHTTPClientOptions will apply http_client_options value to Options
 // HTTPClientOptions sepcify the options for the http client
-func WithhttpClientOptions(v *httpclient.Options) Pair {
+func WithHTTPClientOptions(v *httpclient.Options) Pair {
 	return Pair{
 		Key:   "http_client_options",
 		Value: v,
 	}
 }
 
-// Withinterceptor will apply interceptor value to Options
+// WithInterceptor will apply interceptor value to Options
 // Interceptor
-func Withinterceptor(v Interceptor) Pair {
+func WithInterceptor(v Interceptor) Pair {
 	return Pair{
 		Key:   "interceptor",
 		Value: v,
 	}
 }
 
-// Withlocation will apply location value to Options
+// WithLocation will apply location value to Options
 // Location specify the location for service or storage
-func Withlocation(v string) Pair {
+func WithLocation(v string) Pair {
 	return Pair{
 		Key:   "location",
 		Value: v,
 	}
 }
 
-// Withname will apply name value to Options
+// WithName will apply name value to Options
 // Name specify the storage name
-func Withname(v string) Pair {
+func WithName(v string) Pair {
 	return Pair{
 		Key:   "name",
 		Value: v,
 	}
 }
 
-// Withoffset will apply offset value to Options
+// WithOffset will apply offset value to Options
 // Offset specify offset for this request, storage will seek to this offset before read
-func Withoffset(v int64) Pair {
+func WithOffset(v int64) Pair {
 	return Pair{
 		Key:   "offset",
 		Value: v,
 	}
 }
 
-// WithpairPolicy will apply pair_policy value to Options
+// WithPairPolicy will apply pair_policy value to Options
 // PairPolicy
-func WithpairPolicy(v PairPolicy) Pair {
+func WithPairPolicy(v PairPolicy) Pair {
 	return Pair{
 		Key:   "pair_policy",
 		Value: v,
 	}
 }
 
-// WithreadCallbackFunc will apply read_callback_func value to Options
+// WithReadCallbackFunc will apply read_callback_func value to Options
 // ReadCallbackFunc specify what todo every time we read data from source
-func WithreadCallbackFunc(v func([]byte)) Pair {
+func WithReadCallbackFunc(v func([]byte)) Pair {
 	return Pair{
 		Key:   "read_callback_func",
 		Value: v,
 	}
 }
 
-// Withsize will apply size value to Options
+// WithSize will apply size value to Options
 // Size specify size for this request, storage will only read limited content data
-func Withsize(v int64) Pair {
+func WithSize(v int64) Pair {
 	return Pair{
 		Key:   "size",
 		Value: v,
 	}
 }
 
-// WithstorageClass will apply storage_class value to Options
+// WithStorageClass will apply storage_class value to Options
 // StorageClass
-func WithstorageClass(v string) Pair {
+func WithStorageClass(v string) Pair {
 	return Pair{
 		Key:   "storage_class",
 		Value: v,
 	}
 }
 
-// WithworkDir will apply work_dir value to Options
+// WithWorkDir will apply work_dir value to Options
 // WorkDir specify the work dir for service or storage, every operation will be relative to this dir. work_dir MUST start with / for every storage services. work_dir will be default to / if not set.
 //  For fs storage service on windows platform, the behavior is undefined.
-func WithworkDir(v string) Pair {
+func WithWorkDir(v string) Pair {
 	return Pair{
 		Key:   "work_dir",
 		Value: v,
