09d117a24105e7db16219deb7769fb3a40ecd6c9
Prnyself <281239768@qq.com>
2021-02-19 10:20:15 +0800

    iowrap: Implement CallbackWriter

---
+++ pkg/iowrap/writer.go
@@ -0,0 +1,24 @@
+package iowrap
+
+import "io"
+
+// CallbackWriter will create a new CallbackifyWriter.
+func CallbackWriter(w io.Writer, fn func([]byte)) *CallbackifyWriter {
+	return &CallbackifyWriter{
+		w:  w,
+		fn: fn,
+	}
+}
+
+// CallbackifyWriter will execute callback func in Write.
+type CallbackifyWriter struct {
+	w  io.Writer
+	fn func([]byte)
+}
+
+// Write will write into underlying Writer.
+func (w *CallbackifyWriter) Write(p []byte) (int, error) {
+	n, err := w.w.Write(p)
+	w.fn(p[:n])
+	return n, err
+}
