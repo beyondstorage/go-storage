1cc84e092133c275b9fb8cd0412326ef064d95de
Xuanwo <github@xuanwo.io>
2021-03-03 17:59:11 +0800

    pkg/iowrap: Implement Pipe (#508)
    * pkg/iowrap: Implement Pipe
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Fix build go 1.15
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Fix data race
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Remove extra lock
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Fix small size read
    
    Signed-off-by: Xuanwo <github@xuanwo.io>

---
+++ pkg/iowrap/pipe_bench_test.go
@@ -0,0 +1,77 @@
+package iowrap
+
+import (
+	"io"
+	"io/ioutil"
+	"testing"
+
+	"github.com/aos-dev/go-storage/v3/pkg/randbytes"
+)
+
+func BenchmarkStdPipe(b *testing.B) {
+	cases := []struct {
+		name string
+		size int
+	}{
+		{"4k", 4 * 1024},
+		{"64k", 64 * 1024},
+		{"4m", 4 * 1024 * 1024},
+	}
+
+	for _, v := range cases {
+		b.Run(v.name, func(b *testing.B) {
+			content := make([]byte, v.size)
+			_, err := randbytes.NewRand().Read(content)
+			if err != nil {
+				b.Error(err)
+			}
+
+			r, w := io.Pipe()
+
+			go func() {
+				_, _ = io.Copy(ioutil.Discard, r)
+			}()
+
+			b.SetBytes(int64(v.size))
+			b.StartTimer()
+			for i := 0; i < b.N; i++ {
+				_, _ = w.Write(content)
+			}
+			b.StopTimer()
+		})
+	}
+}
+
+func BenchmarkIowrapPipe(b *testing.B) {
+	cases := []struct {
+		name string
+		size int
+	}{
+		{"4k", 4 * 1024},
+		{"64k", 64 * 1024},
+		{"4m", 4 * 1024 * 1024},
+	}
+
+	for _, v := range cases {
+		b.Run(v.name, func(b *testing.B) {
+			content := make([]byte, v.size)
+			_, err := randbytes.NewRand().Read(content)
+			if err != nil {
+				b.Error(err)
+			}
+
+			r, w := Pipe()
+
+			go func() {
+				_, _ = io.Copy(ioutil.Discard, r)
+			}()
+
+			b.SetBytes(int64(v.size))
+			b.StartTimer()
+			for i := 0; i < b.N; i++ {
+				_, _ = w.Write(content)
+			}
+			b.StopTimer()
+		})
+	}
+}
