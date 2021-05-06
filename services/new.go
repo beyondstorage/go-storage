e11d93f0ddd75dd5ebe2ce474ec2a8b3c39ca569
Xuanwo <github@xuanwo.io>
2021-05-06 16:27:36 +0800

    services: Implement AOS-48 Service Registry (#556)
    Signed-off-by: Xuanwo <github@xuanwo.io>

---
+++ services/new.go
@@ -0,0 +1,70 @@
+package services
+
+import (
+	"errors"
+	"sync"
+
+	"github.com/aos-dev/go-storage/v3/types"
+)
+
+type (
+	// NewServicerFunc is a function that can initiate a new servicer.
+	NewServicerFunc func(ps ...types.Pair) (types.Servicer, error)
+	// NewStoragerFunc is a function that can initiate a new storager.
+	NewStoragerFunc func(ps ...types.Pair) (types.Storager, error)
+)
+
+var (
+	serviceFnMap map[string]NewServicerFunc
+	serviceLock  sync.Mutex
+
+	storagerFnMap map[string]NewStoragerFunc
+	storagerLock  sync.Mutex
+)
+
+// RegisterServicer will register a servicer.
+func RegisterServicer(ty string, fn NewServicerFunc) {
+	serviceLock.Lock()
+	defer serviceLock.Unlock()
+
+	serviceFnMap[ty] = fn
+}
+
+// NewServicer will initiate a new servicer.
+func NewServicer(ty string, ps ...types.Pair) (types.Servicer, error) {
+	serviceLock.Lock()
+	defer serviceLock.Unlock()
+
+	fn, ok := serviceFnMap[ty]
+	if !ok {
+		return nil, errors.New("not registered")
+	}
+
+	return fn(ps...)
+}
+
+// RegisterStorager will register a storager.
+func RegisterStorager(ty string, fn NewStoragerFunc) {
+	storagerLock.Lock()
+	defer storagerLock.Unlock()
+
+	storagerFnMap[ty] = fn
+}
+
+// NewStorager will initiate a new storager.
+func NewStorager(ty string, ps ...types.Pair) (types.Storager, error) {
+	storagerLock.Lock()
+	defer storagerLock.Unlock()
+
+	fn, ok := storagerFnMap[ty]
+	if !ok {
+		return nil, errors.New("not registered")
+	}
+
+	return fn(ps...)
+}
+
+func init() {
+	serviceFnMap = make(map[string]NewServicerFunc)
+	storagerFnMap = make(map[string]NewStoragerFunc)
+}
