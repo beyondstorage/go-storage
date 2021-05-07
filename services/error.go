4b67df27a7f06783b31f475481de47dbb74089fd
Tianxiao Shen <37948597+xxchan@users.noreply.github.com>
2021-05-07 17:35:00 +0800

    Implement AOS-47 Additional Error Specification  (#555)
    * remove `Err` fields
    
    * add ErrUnexpected
    
    * add `ErrObjectNotPart` & `ErrInvalidListMode`
    
    * - replace `ErrObjectNotPart `
    - rename use `<noun><adj>` style
    
    * remove New*Error
    
    * update ModeInvalidErrors
    
    * add ErrNewFuncNotRegistered
    
    * rename `ErrNewFuncNotRegistered` to  `ErrServiceNotRegistered`
    
    * use value receivers
    
    * update ErrUnexpected
    
    * print human readable error for ModeInvalidErrors
    
    * error `struct` should unwrap error code
    
    * InitError should use value instead of pointer

--- services/error.go
+++ services/error.go
@@ -8,6 +8,9 @@ import (
 )
 
 var (
+	// ErrUnexpected means this is an unexpected error which go-storage can't handle
+	ErrUnexpected = errors.New("unexpected")
+
 	// ErrCapabilityInsufficient means this service doesn't have this capability
 	ErrCapabilityInsufficient = errors.New("capability insufficient")
 	// ErrRestrictionDissatisfied means this operation doesn't meat service's restriction.
@@ -15,8 +18,14 @@ var (
 
 	// ErrObjectNotExist means the object to be operated is not exist.
 	ErrObjectNotExist = errors.New("object not exist")
+	// ErrObjectModeInvalid means the provided object mode is invalid.
+	ErrObjectModeInvalid = errors.New("object mode invalid")
 	// ErrPermissionDenied means this operation doesn't have enough permission.
 	ErrPermissionDenied = errors.New("permission denied")
+	// ErrListModeInvalid means the provided list mode is invalid.
+	ErrListModeInvalid = errors.New("list mode invalid")
+	// ErrServiceNotRegistered means this service is not registered.
+	ErrServiceNotRegistered = errors.New("service not registered")
 )
 
 // InitError means this service init failed.
@@ -30,12 +39,12 @@ type InitError struct {
 	Pairs []types.Pair
 }
 
-func (e *InitError) Error() string {
+func (e InitError) Error() string {
 	return fmt.Sprintf("%s %s: %v: %s", e.Type, e.Op, e.Pairs, e.Err.Error())
 }
 
 // Unwrap implements xerrors.Wrapper
-func (e *InitError) Unwrap() error {
+func (e InitError) Unwrap() error {
 	return e.Err
 }
 
@@ -50,7 +59,7 @@ type ServiceError struct {
 	Name string
 }
 
-func (e *ServiceError) Error() string {
+func (e ServiceError) Error() string {
 	if e.Name == "" {
 		return fmt.Sprintf("%s: %s: %s", e.Op, e.Servicer, e.Err.Error())
 	}
@@ -58,7 +67,7 @@ func (e *ServiceError) Error() string {
 }
 
 // Unwrap implements xerrors.Wrapper
-func (e *ServiceError) Unwrap() error {
+func (e ServiceError) Unwrap() error {
 	return e.Err
 }
 
@@ -73,7 +82,7 @@ type StorageError struct {
 	Path []string
 }
 
-func (e *StorageError) Error() string {
+func (e StorageError) Error() string {
 	if e.Path == nil {
 		return fmt.Sprintf("%s: %s: %s", e.Op, e.Storager, e.Err.Error())
 	}
@@ -81,80 +90,76 @@ func (e *StorageError) Error() string {
 }
 
 // Unwrap implements xerrors.Wrapper
-func (e *StorageError) Unwrap() error {
+func (e StorageError) Unwrap() error {
 	return e.Err
-}
-
-// NewMetadataNotRecognizedError will create a new MetadataUnrecognizedError.
-func NewMetadataNotRecognizedError(key string, value interface{}) *MetadataUnrecognizedError {
-	return &MetadataUnrecognizedError{
-		Err:   ErrCapabilityInsufficient,
-		Key:   key,
-		Value: value,
-	}
 }
 
 // MetadataUnrecognizedError means this operation meets unrecognized metadata.
 type MetadataUnrecognizedError struct {
-	Err error
-
 	Key   string
 	Value interface{}
 }
 
-func (e *MetadataUnrecognizedError) Error() string {
-	return fmt.Sprintf("metadata unrecognized, %s, %v: %s", e.Key, e.Value, e.Err.Error())
+func (e MetadataUnrecognizedError) Error() string {
+	return fmt.Sprintf("metadata unrecognized, %s, %v: %s", e.Key, e.Value, ErrCapabilityInsufficient.Error())
 }
 
 // Unwrap implements xerrors.Wrapper
-func (e *MetadataUnrecognizedError) Unwrap() error {
-	return e.Err
-}
-
-// NewPairUnsupportedError will create a new PairUnsupportedError.
-func NewPairUnsupportedError(pair types.Pair) *PairUnsupportedError {
-	return &PairUnsupportedError{
-		Err:  ErrCapabilityInsufficient,
-		Pair: pair,
-	}
+func (e MetadataUnrecognizedError) Unwrap() error {
+	return ErrCapabilityInsufficient
 }
 
 // PairUnsupportedError means this operation has unsupported pair.
 type PairUnsupportedError struct {
-	Err error
-
 	Pair types.Pair
 }
 
-func (e *PairUnsupportedError) Error() string {
-	return fmt.Sprintf("pair unsupported, %s: %s", e.Pair, e.Err.Error())
+func (e PairUnsupportedError) Error() string {
+	return fmt.Sprintf("pair unsupported, %s: %s", e.Pair, ErrCapabilityInsufficient.Error())
 }
 
 // Unwrap implements xerrors.Wrapper
-func (e *PairUnsupportedError) Unwrap() error {
-	return e.Err
-}
-
-// NewPairRequiredError will create a new PairRequiredError.
-func NewPairRequiredError(keys ...string) *PairRequiredError {
-	return &PairRequiredError{
-		Err:  ErrRestrictionDissatisfied,
-		Keys: keys,
-	}
+func (e PairUnsupportedError) Unwrap() error {
+	return ErrCapabilityInsufficient
 }
 
 // PairRequiredError means this operation has required pair but missing.
 type PairRequiredError struct {
-	Err error
-
 	Keys []string
 }
 
-func (e *PairRequiredError) Error() string {
-	return fmt.Sprintf("pair required, %v: %s", e.Keys, e.Err.Error())
+func (e PairRequiredError) Error() string {
+	return fmt.Sprintf("pair required, %v: %s", e.Keys, ErrRestrictionDissatisfied.Error())
 }
 
 // Unwrap implements xerrors.Wrapper
-func (e *PairRequiredError) Unwrap() error {
-	return e.Err
+func (e PairRequiredError) Unwrap() error {
+	return ErrRestrictionDissatisfied
+}
+
+// ObjectModeInvalidError means the provided object mode is invalid.
+type ObjectModeInvalidError struct {
+	Expected types.ObjectMode
+	Actual   types.ObjectMode
+}
+
+func (e ObjectModeInvalidError) Error() string {
+	return fmt.Sprintf("object mode invalid, expected %v, actual %v", e.Expected, e.Actual)
+}
+
+func (e ObjectModeInvalidError) Unwrap() error {
+	return ErrObjectModeInvalid
+}
+
+// ListModeInvalidError means the provided list mode is invalid.
+type ListModeInvalidError struct {
+	Actual types.ListMode
+}
+
+func (e ListModeInvalidError) Error() string {
+	return fmt.Sprintf("list mode invalid, actual %v", e.Actual)
+}
+
+func (e ListModeInvalidError) Unwrap() error {
+	return ErrListModeInvalid
 }
