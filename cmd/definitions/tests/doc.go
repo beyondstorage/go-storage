32b2db7e995c12065b34d7162d751c4bf26272d9
Lance <281239768@qq.com>
2021-05-10 17:47:25 +0800

    docs: Add comment to illustrate ignoring generated.go in tests package (#567)
    * doc: Add comment to illustrate ignoring generated.go in tests package
    
    Signed-off-by: Prnyself <281239768@qq.com>
    
    * fix comment style
    
    Signed-off-by: Prnyself <281239768@qq.com>

--- cmd/definitions/tests/doc.go
+++ cmd/definitions/tests/doc.go
@@ -1,3 +1,9 @@
+/*
+Package tests need to ensure the generator work as intended so that we can generate code correctly.
+So we ignored `generated.go`, and generated every time to test generator and check `service.toml`.
+If the test failed, the generator SHOULD NOT be used in specific service.
+*/
+
 package tests
 
 //go:generate go run -tags tools github.com/aos-dev/go-storage/v3/cmd/definitions service.toml
