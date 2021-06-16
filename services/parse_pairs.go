db25b673c7a3e6e8d9bf44b01f182e5cb957d7c3
xxchan <xxchan@sjtu.edu.cn>
2021-06-16 15:38:53 +0800

    implement config string

--- services/parse_pairs.go
+++ services/parse_pairs.go
@@ -1,11 +1,13 @@
 package services
 
 import (
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"strconv"
 	"strings"
 
+	"github.com/beyondstorage/go-storage/v4/pairs"
 	. "github.com/beyondstorage/go-storage/v4/types"
 )
 
@@ -16,10 +18,71 @@ var (
 	ErrPairNotRegistered = NewErrorCode("pair not registered")
 	// ErrPairValueInvalid means the pair's value is invalid.
 	ErrPairValueInvalid = NewErrorCode("pair value invalid")
+	// ErrConfigStringInvalid means the config string is invalid
+	ErrConfigStringInvalid = NewErrorCode("config string is invalid")
 )
 
+// <type>://[<name>][<work_dir>][?key1=value1&...&keyN=valueN]
 func parseString(config string) (ty string, ps []Pair, err error) {
-	return "", nil, nil
+	colon := strings.Index(config, ":")
+	if colon == -1 {
+		err = fmt.Errorf("%w: %s, %s", ErrConfigStringInvalid, "service type missing", config)
+		return
+	}
+	ty = config[:colon]
+	rest := config[colon+1:]
+	m, ok := servicePairMaps[ty]
+	if !ok {
+		err = ErrServiceNotRegistered
+		return
+	}
+
+	if !strings.HasPrefix(rest, "//") {
+		err = fmt.Errorf("%w: %s", ErrConfigStringInvalid, config)
+		return
+	}
+	rest = rest[2:]
+
+	// [<name>][<work_dir>][?key1=value1&...&keyN=valueN]
+	// <name> does not contain '/'
+	// <work_dir> begins with '/'
+	question := strings.Index(rest, "?")
+	var path string
+	if question == -1 {
+		path = rest
+		rest = ""
+	} else {
+		path = rest[:question]
+		rest = rest[question+1:]
+	}
+
+	if len(path) == 0 {
+		err = fmt.Errorf("%w: %s, %s", ErrConfigStringInvalid, "both <name> and <work_dir> missing", config)
+		return
+	} else {
+		slash := strings.Index(path, "/")
+		if slash == -1 {
+			name := path
+			ps = append(ps, pairs.WithName(name))
+		} else if slash == 0 {
+			work_dir := path
+			ps = append(ps, pairs.WithWorkDir(work_dir))
+		} else {
+			name := path[:slash]
+			work_dir := path[slash:]
+			ps = append(ps, pairs.WithName(name), pairs.WithWorkDir(work_dir))
+		}
+	}
+
+	for _, v := range strings.Split(rest, "&") {
+		opt := strings.SplitN(v, "=", 2)
+		if len(opt) != 2 {
+			// && or &key& or &key=&, ignore
+			continue
+		}
+		parse(m, opt[0], opt[1])
+	}
+	return
 }
 
 func parse(m map[string]string, k string, v string) (pair Pair, err error) {
@@ -42,6 +105,10 @@ func parse(m map[string]string, k string, v string) (pair Pair, err error) {
 		pair.Value = int(i)
 	case "int64":
 		pair.Value, err = strconv.ParseInt(v, 0, 64)
+	case "[]byte":
+		pair.Value, err = base64.RawStdEncoding.DecodeString(v)
+	case "ListMode":
+		pair.Value, err = parseListMode(v)
 	default:
 		return Pair{}, fmt.Errorf("%w: %v, %v", ErrPairTypeNotParsable, k, vType)
 	}
@@ -54,10 +121,6 @@ func parse(m map[string]string, k string, v string) (pair Pair, err error) {
 }
 
 func parseListMode(s string) (ListMode, error) {
-	v64, err := strconv.ParseUint(s, 0, 8)
-	if err == nil || errors.Is(err, strconv.ErrRange) {
-		return ListMode(v64), nil
-	}
 	if strings.TrimSpace(s) == "" {
 		return ListMode(0), nil
 	}
