fd534eafccc9d8b5edb0bf54f5e6fd6e0f7b55e2
Jun <jun@junz.org>
2021-07-05 19:07:47 +0800

    services: Implement GSP-127: Add ServiceInternal and RequestThrottled Errors (#612)

--- services/error.go
+++ services/error.go
@@ -52,6 +52,10 @@ var (
 	ErrListModeInvalid = NewErrorCode("list mode invalid")
 	// ErrServiceNotRegistered means this service is not registered.
 	ErrServiceNotRegistered = NewErrorCode("service not registered")
+	//ErrServiceInternal means this service has an internal error.
+	ErrServiceInternal = NewErrorCode("service internal error")
+	//ErrRequestThrottled means there are too many requests.
+	ErrRequestThrottled = NewErrorCode("too many requests")
 )
 
 // InitError means this service init failed.
