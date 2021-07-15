ba29fada764364c4eb379d8e13b1521e009ca15f
Xuanwo <github@xuanwo.io>
2021-07-15 15:53:00 +0800

    cmd: Add warning for type convert instead (#670)
    * cmd: Add warning for type convert instead
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * cmd: Add docs for parseType
    
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- cmd/definitions/parse.go
+++ cmd/definitions/parse.go
@@ -61,3 +61,36 @@ func parseFunc(name string) map[string]*templateutils.Method {
 	}
 	return data
 }
+
+var typeMap = map[string]string{
+	"context":             "context.Context",
+	"http_client_options": "*httpclient.Options",
+
+	"any":               "interface{}",
+	"byte_array":        "[]byte",
+	"string_array":      "[]string",
+	"string_string_map": "map[string]string",
+	"time":              "time.Time",
+	"BlockIterator":     "*BlockIterator",
+	"IoCallback":        "func([]byte)",
+	"Object":            "*Object",
+	"ObjectIterator":    "*ObjectIterator",
+	"Pairs":             "...Pair",
+	"Part":              "*Part",
+	"Parts":             "[]*Part",
+	"PartIterator":      "*PartIterator",
+	"Reader":            "io.Reader",
+	"StoragerIterator":  "*StoragerIterator",
+	"StorageMeta":       "*StorageMeta",
+	"Writer":            "io.Writer",
+}
+
+// TODO: We can remove this convert after all service migrated.
+func parseType(v string) string {
+	s, ok := typeMap[v]
+	if !ok {
+		return v
+	}
+	log.Warnf("type %s is not supported anymore, please updated to %s.", v, s)
+	return s
+}
