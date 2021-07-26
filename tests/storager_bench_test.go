b6e9d61ac6dac23c2dea78f13e6907cdc34deb0b
Xuanwo <github@xuanwo.io>
2021-07-26 12:09:55 +0800

    tests: Implement bench for List (#687)
    * tests: Implement bench for List
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Update storager_bench_test.go
    
    * tests: Fix format
    
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- tests/storager_bench_test.go
+++ tests/storager_bench_test.go
@@ -1,6 +1,11 @@
 package tests
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/beyondstorage/go-storage/v4/types"
+)
 
 func BenchmarkStorage_Stat(b *testing.B) {
 	s, err := NewStorager()
@@ -10,5 +15,27 @@ func BenchmarkStorage_Stat(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		_, _ = s.Stat("abc")
+	}
+}
+
+func BenchmarkStorage_List(b *testing.B) {
+	ctx := context.TODO()
+
+	var ob []*types.Object
+	for i := 0; i < 1024; i++ {
+		ob = append(ob, &types.Object{})
+	}
+
+	s := &Storage{
+		objects: ob,
+	}
+
+	it, err := s.list(ctx, "", pairStorageList{})
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		_, _ = it.Next()
 	}
 }
