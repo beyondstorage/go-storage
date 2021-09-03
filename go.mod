7a87b5aeab183fc371ce485afa169758cbda9f54
Xuanwo <github@xuanwo.io>
2021-09-03 09:32:40 +0800

    Use Xuanwo/gg to play good game
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- go.mod
+++ go.mod
@@ -3,18 +3,13 @@ module github.com/beyondstorage/go-storage/v4
 go 1.15
 
 require (
+	github.com/Xuanwo/gg v0.0.0-20210903012417-03a447370afa
 	github.com/Xuanwo/templateutils v0.1.0
 	github.com/dave/dst v0.26.2
-	github.com/dave/jennifer v1.2.0
 	github.com/golang/mock v1.6.0
 	github.com/google/uuid v1.3.0
 	github.com/kevinburke/go-bindata v3.22.0+incompatible
 	github.com/pelletier/go-toml v1.9.3
 	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/testify v1.7.0
-	github.com/Xuanwo/go-codegen v1.0.0
-)
-
-replace (
-	github.com/Xuanwo/go-codegen   => ../../xuanwo/go-codegen
 )
--- pairs/generated.go
+++ pairs/generated.go
@@ -10,167 +10,131 @@ import (
 	. "github.com/beyondstorage/go-storage/v4/types"
 )
 
-/*
-WithContentMd5 will apply content_md5 value to Options.
-
-ContentMd5
-*/
+// WithContentMd5 will apply content_md5 value to Options.
+//
+// ContentMd5
 func WithContentMd5(v string) (p Pair) {
 	return Pair{Key: "content_md5", Value: v}
 }
 
-/*
-WithContentType will apply content_type value to Options.
-
-ContentType
-*/
+// WithContentType will apply content_type value to Options.
+//
+// ContentType
 func WithContentType(v string) (p Pair) {
 	return Pair{Key: "content_type", Value: v}
 }
 
-/*
-WithContext will apply context value to Options.
-
-Context
-*/
+// WithContext will apply context value to Options.
+//
+// Context
 func WithContext(v context.Context) (p Pair) {
 	return Pair{Key: "context", Value: v}
 }
 
-/*
-WithContinuationToken will apply continuation_token value to Options.
-
-ContinuationToken specify the continuation token for list
-*/
+// WithContinuationToken will apply continuation_token value to Options.
+//
+// ContinuationToken specify the continuation token for list
 func WithContinuationToken(v string) (p Pair) {
 	return Pair{Key: "continuation_token", Value: v}
 }
 
-/*
-WithCredential will apply credential value to Options.
-
-Credential specify how to provide credential for service or storage
-*/
+// WithCredential will apply credential value to Options.
+//
+// Credential specify how to provide credential for service or storage
 func WithCredential(v string) (p Pair) {
 	return Pair{Key: "credential", Value: v}
 }
 
-/*
-WithEndpoint will apply endpoint value to Options.
-
-Endpoint specify how to provide endpoint for service or storage
-*/
+// WithEndpoint will apply endpoint value to Options.
+//
+// Endpoint specify how to provide endpoint for service or storage
 func WithEndpoint(v string) (p Pair) {
 	return Pair{Key: "endpoint", Value: v}
 }
 
-/*
-WithExpire will apply expire value to Options.
-
-Expire specify when the url returned by reach will expire
-*/
+// WithExpire will apply expire value to Options.
+//
+// Expire specify when the url returned by reach will expire
 func WithExpire(v time.Duration) (p Pair) {
 	return Pair{Key: "expire", Value: v}
 }
 
-/*
-WithHTTPClientOptions will apply http_client_options value to Options.
-
-HTTPClientOptions
-*/
+// WithHTTPClientOptions will apply http_client_options value to Options.
+//
+// HTTPClientOptions
 func WithHTTPClientOptions(v *httpclient.Options) (p Pair) {
 	return Pair{Key: "http_client_options", Value: v}
 }
 
-/*
-WithInterceptor will apply interceptor value to Options.
-
-Interceptor
-*/
+// WithInterceptor will apply interceptor value to Options.
+//
+// Interceptor
 func WithInterceptor(v Interceptor) (p Pair) {
 	return Pair{Key: "interceptor", Value: v}
 }
 
-/*
-WithIoCallback will apply io_callback value to Options.
-
-IoCallback specify what todo every time we read data from source
-*/
+// WithIoCallback will apply io_callback value to Options.
+//
+// IoCallback specify what todo every time we read data from source
 func WithIoCallback(v func([]byte)) (p Pair) {
 	return Pair{Key: "io_callback", Value: v}
 }
 
-/*
-WithListMode will apply list_mode value to Options.
-
-ListMode
-*/
+// WithListMode will apply list_mode value to Options.
+//
+// ListMode
 func WithListMode(v ListMode) (p Pair) {
 	return Pair{Key: "list_mode", Value: v}
 }
 
-/*
-WithLocation will apply location value to Options.
-
-Location specify the location for service or storage
-*/
+// WithLocation will apply location value to Options.
+//
+// Location specify the location for service or storage
 func WithLocation(v string) (p Pair) {
 	return Pair{Key: "location", Value: v}
 }
 
-/*
-WithMultipartID will apply multipart_id value to Options.
-
-MultipartID
-*/
+// WithMultipartID will apply multipart_id value to Options.
+//
+// MultipartID
 func WithMultipartID(v string) (p Pair) {
 	return Pair{Key: "multipart_id", Value: v}
 }
 
-/*
-WithName will apply name value to Options.
-
-Name specify the storage name
-*/
+// WithName will apply name value to Options.
+//
+// Name specify the storage name
 func WithName(v string) (p Pair) {
 	return Pair{Key: "name", Value: v}
 }
 
-/*
-WithObjectMode will apply object_mode value to Options.
-
-ObjectMode ObjectMode hint
-*/
+// WithObjectMode will apply object_mode value to Options.
+//
+// ObjectMode ObjectMode hint
 func WithObjectMode(v ObjectMode) (p Pair) {
 	return Pair{Key: "object_mode", Value: v}
 }
 
-/*
-WithOffset will apply offset value to Options.
-
-Offset specify offset for this request, storage will seek to this offset before read
-*/
+// WithOffset will apply offset value to Options.
+//
+// Offset specify offset for this request, storage will seek to this offset before read
 func WithOffset(v int64) (p Pair) {
 	return Pair{Key: "offset", Value: v}
 }
 
-/*
-WithSize will apply size value to Options.
-
-Size specify size for this request, storage will only read limited content data
-*/
+// WithSize will apply size value to Options.
+//
+// Size specify size for this request, storage will only read limited content data
 func WithSize(v int64) (p Pair) {
 	return Pair{Key: "size", Value: v}
 }
 
-/*
-WithWorkDir will apply work_dir value to Options.
-
-WorkDir specify the work dir for service or storage, every operation will be relative to this dir.
-work_dir MUST start with / for every storage services.
-work_dir will be default to / if not set.
-For fs storage service on windows platform, the behavior is undefined.
-*/
+// WithWorkDir will apply work_dir value to Options.
+//
+// WorkDir specify the work dir for service or storage, every operation will be relative to this dir.
+// work_dir MUST start with / for every storage services.
+// work_dir will be default to / if not set.
+// For fs storage service on windows platform, the behavior is undefined.
 func WithWorkDir(v string) (p Pair) {
 	return Pair{Key: "work_dir", Value: v}
 }
