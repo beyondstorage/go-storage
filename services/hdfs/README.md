c86e76d78639fd7d49f631905c8e84412a3f6416
1445601153 <1445601153@qq.com>
2021-10-19 16:29:46 +0800

    Move hdfs back

---
+++ services/hdfs/README.md
@@ -0,0 +1,33 @@
+# hdfs 
+
+Hadoop Distributed File System (HDFS) support for [go-storage](https://github.com/beyondstorage/go-storage).
+
+## Install
+
+```go
+go get go.beyondstorage.io/services/hdfs
+```
+
+## Usage
+
+```go
+import (
+	"log"
+	
+	_ "go.beyondstorage.io/services/hdfs"
+	"go.beyondstorage.io/v5/services"
+)
+
+func main() {
+	store, err := services.NewStoragerFromString("hdfs:///path/to/workdir?endpoint=tcp:<host>:<port>")
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	// Write data from io.Reader into hello.txt
+	n, err := store.Write("hello.txt", r, length)
+}
+```
+
+- See more examples in [go-storage-example](https://github.com/beyondstorage/go-storage-example).
+- Read [more docs](https://beyondstorage.io/docs/go-storage/services/hdfs) about go-service-hdfs.
---
+++ services/hdfs/readdir.go
@@ -0,0 +1,63 @@
+package hdfs
+
+import (
+	"context"
+	"errors"
+	"io"
+
+	"github.com/colinmarc/hdfs/v2"
+	"go.beyondstorage.io/v5/types"
+)
+
+const defaultListObjectLimit = 100
+
+type listDirInput struct {
+	rp  string
+	dir *hdfs.FileReader
+
+	continuationToken string
+}
+
+func (i *listDirInput) ContinuationToken() string {
+	return i.continuationToken
+}
+
+func (s *Storage) listDirNext(ctx context.Context, page *types.ObjectPage) (err error) {
+	input := page.Status.(*listDirInput)
+
+	if input.dir == nil {
+		input.dir, err = s.hdfs.Open(input.rp)
+		if err != nil {
+			return
+		}
+	}
+
+	fileList, err := input.dir.Readdir(defaultListObjectLimit)
+
+	if err != nil && errors.Is(err, io.EOF) {
+		_ = input.dir.Close()
+		input.dir = nil
+		return types.IterateDone
+	}
+
+	for _, f := range fileList {
+		o := s.newObject(true)
+		o.ID = input.rp
+		o.Path = f.Name()
+
+		if f.Mode().IsDir() {
+			o.Mode |= types.ModeDir
+		}
+
+		if f.Mode().IsRegular() {
+			o.Mode |= types.ModeRead
+		}
+
+		o.SetContentLength(f.Size())
+
+		page.Data = append(page.Data, o)
+		input.continuationToken = o.Path
+	}
+
+	return
+}
