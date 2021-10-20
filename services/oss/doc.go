9e1f50d77337b1d695081cb044c574a32abaa294
1445601153 <1445601153@qq.com>
2021-10-20 17:09:41 +0800

    upgrade go version to 1.16

--- services/oss/doc.go
+++
@@ -1,6 +0,0 @@
-/*
-Package oss provided support for Aliyun Object Storage Service (https://cn.aliyun.com/product/oss)
-*/
-package oss
-
-//go:generate go run -tags tools go.beyondstorage.io/v5/cmd/definitions service.toml
--- services/oss/tools.go
+++
@@ -1,5 +0,0 @@
-//+build tools
-
-package oss
-
-import _ "github.com/beyondstorage/go-storage/v4/cmd/definitions"
