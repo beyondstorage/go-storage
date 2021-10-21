64d108b7cb504c60992ca6bfb3a0aba427be66da
Joey-1445601153 <1445601153@qq.com>
2021-10-21 17:01:48 +0800

    Move zip back

---
+++ services/zip/doc.go
@@ -0,0 +1,6 @@
+/*
+Package zip provided support for zip archive files.
+*/
+package zip
+
+//go:generate go run -tags tools go.beyondstorage.io/v5/cmd/definitions service.toml
---
+++ services/zip/go.mod
@@ -0,0 +1,5 @@
+module go.beyondstorage.io/v5/zip
+
+go 1.16
+
+require github.com/beyondstorage/go-storage/v4 v4.8.0
