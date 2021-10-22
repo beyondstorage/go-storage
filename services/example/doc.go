83e2cdf5b13aef53d4c43bd9befa7b09399d017e
Joey-1445601153 <1445601153@qq.com>
2021-10-22 14:19:51 +0800

    Add example back

---
+++ services/example/doc.go
@@ -0,0 +1,6 @@
+/*
+Package example provided support for local file system.
+*/
+package example
+
+//go:generate go run -tags tools  go.beyondstorage.io/v5/cmd/definitions service.toml
---
+++ services/example/go.mod
@@ -0,0 +1,8 @@
+module go.beyondstorage.io/services/example
+
+go 1.16
+
+require (
+	github.com/beyondstorage/go-storage/v4 v4.8.0
+	go.beyondstorage.io/v5 v5.0.0
+)
