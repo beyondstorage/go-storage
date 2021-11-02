2df7c358bf15fc7ef15bddc0909a82a81c3644a2
Xuanwo <github@xuanwo.io>
2021-11-02 14:01:14 +0800

    Remove generate for iowrap
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- pkg/iowrap/doc.go
+++ pkg/iowrap/doc.go
@@ -1,7 +1,3 @@
 // Package iowrap intends to provide io wrappers that useful.
 
 package iowrap
-
-import _ "github.com/golang/mock/mockgen/model"
-
-//go:generate go run github.com/golang/mock/mockgen@v1.6.0 -package iowrap -destination mock_test.go io Reader,Closer,ReaderAt,Seeker,Writer
