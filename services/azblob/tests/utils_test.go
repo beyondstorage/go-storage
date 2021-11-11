dc7b7249ccd9d163149bb46b25c92bf2808bd7f5
Joey2132 <jiayu2132@163.com>
2021-11-11 17:13:42 +0800

    services/azblob: Migrate to Factory init

--- services/azblob/tests/utils_test.go
+++ services/azblob/tests/utils_test.go
@@ -18,6 +18,7 @@ func setupTest(t *testing.T) types.Storager {
 		ps.WithName(os.Getenv("STORAGE_AZBLOB_NAME")),
 		ps.WithEndpoint(os.Getenv("STORAGE_AZBLOB_ENDPOINT")),
 		ps.WithWorkDir("/"+uuid.New().String()+"/"),
+		ps.WithEnableVirtualDir(),
 	)
 	if err != nil {
 		t.Errorf("new storager: %v", err)
