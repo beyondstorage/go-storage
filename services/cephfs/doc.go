ce097f70b8ed47f5018f9f97651f3d5462153cb3
Joey2132 <jiayu2132@163.com>
2021-11-15 15:32:14 +0800

    services/cephfs:Migrate to Factory init

--- services/cephfs/doc.go
+++ services/cephfs/doc.go
@@ -3,4 +3,4 @@ Package cephfs provided support for ceph file system(https://ceph.io).
 */
 package cephfs
 
-//go:generate go run -tags tools go.beyondstorage.io/v5/cmd/definitions service.toml
+//go:generate go run ./internal/cmd
