d9d823bdec9e8227b466741ccac5e33581d66ceb
Joey2132 <jiayu2132@163.com>
2021-11-17 16:18:38 +0800

    services/gcs:Migrate to Factory init

--- services/gcs/tests/utils_test.go
+++ services/gcs/tests/utils_test.go
@@ -19,9 +19,7 @@ func setupTest(t *testing.T) types.Storager {
 		ps.WithName(os.Getenv("STORAGE_GCS_NAME")),
 		ps.WithWorkDir("/"+uuid.New().String()+"/"),
 		gcs.WithProjectID(os.Getenv("STORAGE_GCS_PROJECT_ID")),
-		gcs.WithStorageFeatures(gcs.StorageFeatures{
-			VirtualDir: true,
-		}),
+		ps.WithEnableVirtualDir(),
 	)
 	if err != nil {
 		t.Errorf("new storager: %v", err)
