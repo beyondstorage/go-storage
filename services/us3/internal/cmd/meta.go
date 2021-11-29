2fc19e492f6970541f3a7d021087e39a6533ecc7
Joey2132 <jiayu2132@163.com>
2021-11-29 16:10:50 +0800

    services/us3: Migrate to Factory init.

---
+++ services/us3/internal/cmd/meta.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	def "go.beyondstorage.io/v5/definitions"
+	"go.beyondstorage.io/v5/types"
+)
+
+var Metadata = def.Metadata{
+	Name:  "us3",
+	Infos: []def.Info{},
+	Pairs: []def.Pair{},
+	Factory: []def.Pair{
+		def.PairWorkDir,
+	},
+	Service: def.Service{
+		Features: types.ServiceFeatures{},
+	},
+	Storage: def.Storage{
+		Features: types.StorageFeatures{
+			WriteEmptyObject: true,
+
+			Create:   true,
+			Delete:   true,
+			Metadata: true,
+			List:     true,
+			Read:     true,
+			Stat:     true,
+			Write:    true,
+		},
+
+		Create: []def.Pair{
+			def.PairObjectMode,
+		},
+		Delete: []def.Pair{
+			def.PairObjectMode,
+		},
+		List: []def.Pair{
+			def.PairListMode,
+		},
+		Read: []def.Pair{
+			def.PairOffset,
+			def.PairIoCallback,
+			def.PairSize,
+		},
+		Write: []def.Pair{
+			def.PairContentMD5,
+			def.PairContentType,
+			def.PairIoCallback,
+		},
+		Stat: []def.Pair{
+			def.PairObjectMode,
+		},
+		CreateDir: []def.Pair{},
+	},
+}
