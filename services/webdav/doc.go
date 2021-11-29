a6e3a991e8dbcac4ef5496841a48d6a1ac061c4e
Joey2132 <jiayu2132@163.com>
2021-11-29 16:47:12 +0800

    services/webdav: Migrate to Factory init

--- services/webdav/doc.go
+++ services/webdav/doc.go
@@ -3,4 +3,4 @@ Package webdav provided support for webdav archive files.
 */
 package webdav
 
-//go:generate go run -tags tools go.beyondstorage.io/v5/cmd/definitions service.toml
+//go:generate go run ./internal/cmd
---
+++ services/webdav/internal/cmd/meta.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	def "go.beyondstorage.io/v5/definitions"
+	"go.beyondstorage.io/v5/types"
+)
+
+var Metadata = def.Metadata{
+	Name:  "webdav",
+	Pairs: []def.Pair{},
+	Infos: []def.Info{},
+	Factory: []def.Pair{
+		def.PairWorkDir,
+	},
+	Service: def.Service{},
+	Storage: def.Storage{
+		Features: types.StorageFeatures{
+			WriteEmptyObject: true,
+
+			Create:   true,
+			Delete:   true,
+			List:     true,
+			Metadata: true,
+			Read:     true,
+			Write:    true,
+		},
+		Create: []def.Pair{
+			def.PairObjectMode,
+		},
+		Delete: []def.Pair{
+			def.PairObjectMode,
+		},
+		List: []def.Pair{
+			def.PairListMode,
+		},
+		Read: []def.Pair{
+			def.PairOffset,
+			def.PairIoCallback,
+			def.PairSize,
+		},
+		Write: []def.Pair{
+			def.PairContentMD5,
+			def.PairContentType,
+			def.PairIoCallback,
+		},
+		Stat: []def.Pair{
+			def.PairObjectMode,
+		},
+	},
+}
