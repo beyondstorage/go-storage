c74b287a6f7d3e22d97acbc23459aa99491b8f4e
Joey2132 <jiayu2132@163.com>
2021-11-29 17:33:48 +0800

    services/zip: Migrate to Factory init

--- services/zip/doc.go
+++ services/zip/doc.go
@@ -3,4 +3,4 @@ Package zip provided support for zip archive files.
 */
 package zip
 
-//go:generate go run -tags tools go.beyondstorage.io/v5/cmd/definitions service.toml
+//go:generate go run ./internal/cmd
--- services/zip/go.mod
+++ services/zip/go.mod
@@ -3,3 +3,5 @@ module go.beyondstorage.io/v5/zip
 go 1.16
 
 require go.beyondstorage.io/v5 v5.0.0
+
+replace go.beyondstorage.io/v5 => ../../
