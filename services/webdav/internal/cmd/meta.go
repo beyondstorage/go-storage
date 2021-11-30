f0e1f0c48aac86937a3475d95bbf24eaba1eb816
Joey2132 <jiayu2132@163.com>
2021-11-30 12:56:52 +0800

    Delete not implemented func

--- services/webdav/internal/cmd/meta.go
+++ services/webdav/internal/cmd/meta.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	def "go.beyondstorage.io/v5/definitions"
-	"go.beyondstorage.io/v5/types"
 )
 
 var Metadata = def.Metadata{
@@ -13,38 +12,5 @@ var Metadata = def.Metadata{
 		def.PairWorkDir,
 	},
 	Service: def.Service{},
-	Storage: def.Storage{
-		Features: types.StorageFeatures{
-			WriteEmptyObject: true,
-
-			Create:   true,
-			Delete:   true,
-			List:     true,
-			Metadata: true,
-			Read:     true,
-			Write:    true,
-		},
-		Create: []def.Pair{
-			def.PairObjectMode,
-		},
-		Delete: []def.Pair{
-			def.PairObjectMode,
-		},
-		List: []def.Pair{
-			def.PairListMode,
-		},
-		Read: []def.Pair{
-			def.PairOffset,
-			def.PairIoCallback,
-			def.PairSize,
-		},
-		Write: []def.Pair{
-			def.PairContentMD5,
-			def.PairContentType,
-			def.PairIoCallback,
-		},
-		Stat: []def.Pair{
-			def.PairObjectMode,
-		},
-	},
+	Storage: def.Storage{},
 }
