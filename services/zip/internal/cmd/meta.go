96473d54133cde83b502464fca1f7f30b6e2b6c4
Joey2132 <jiayu2132@163.com>
2021-11-30 12:58:14 +0800

    Delete not implemented func

--- services/zip/internal/cmd/meta.go
+++ services/zip/internal/cmd/meta.go
@@ -14,37 +14,6 @@ var Metadata = def.Metadata{
 	},
 	Service: def.Service{},
 	Storage: def.Storage{
-		Features: types.StorageFeatures{
-			WriteEmptyObject: true,
-
-			Create:   true,
-			Delete:   true,
-			List:     true,
-			Metadata: true,
-			Read:     true,
-			Write:    true,
-		},
-		Create: []def.Pair{
-			def.PairObjectMode,
-		},
-		Delete: []def.Pair{
-			def.PairObjectMode,
-		},
-		List: []def.Pair{
-			def.PairListMode,
-		},
-		Read: []def.Pair{
-			def.PairOffset,
-			def.PairIoCallback,
-			def.PairSize,
-		},
-		Write: []def.Pair{
-			def.PairContentMD5,
-			def.PairContentType,
-			def.PairIoCallback,
-		},
-		Stat: []def.Pair{
-			def.PairObjectMode,
-		},
+		Features: types.StorageFeatures{},
 	},
 }
