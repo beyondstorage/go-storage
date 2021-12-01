41c04a74693b6ac1e51d4e1de09779c81181826f
Joey2132 <jiayu2132@163.com>
2021-12-01 15:13:32 +0800

    Test

--- services/fs/internal/cmd/meta.go
+++ services/fs/internal/cmd/meta.go
@@ -2,65 +2,61 @@ package main
 
 import (
 	def "go.beyondstorage.io/v5/definitions"
-	"go.beyondstorage.io/v5/types"
+	// "go.beyondstorage.io/v5/types"
 )
 
 var Metadata = def.Metadata{
-	Name: "fs",
-	Pairs: []def.Pair{
-		pairContinuationToken,
-	},
+	Name:  "fs",
+	Pairs: []def.Pair{},
 	Infos: []def.Info{},
 	Factory: []def.Pair{
 		def.PairWorkDir,
 	},
 	Service: def.Service{},
 	Storage: def.Storage{
-		Features: types.StorageFeatures{
-			WriteEmptyObject: true,
+		// Features: types.StorageFeatures{
+		// 	WriteEmptyObject: true,
 
-			Create:       true,
-			CreateDir:    true,
-			CreateAppend: true,
-			Delete:       true,
-			List:         true,
-			Metadata:     true,
-			Read:         true,
-			Stat:         true,
-			Write:        true,
-			WriteAppend:  true,
-			CommitAppend: true,
-		},
+		// 	Copy:         true,
+		// 	Create:       true,
+		// 	CreateAppend: true,
+		// 	CreateDir:    true,
+		// 	CreateLink:   true,
+		// 	Delete:       true,
+		// 	Fetch:        true,
+		// 	List:         true,
+		// 	Metadata:     true,
+		// 	Move:         true,
+		// 	Read:         true,
+		// 	Stat:         true,
+		// 	Write:        true,
+		// 	WriteAppend:  true,
+		// 	CommitAppend: true,
+		// },
 
-		Create: []def.Pair{
-			def.PairObjectMode,
-		},
-		Delete: []def.Pair{
-			def.PairObjectMode,
-		},
-		List: []def.Pair{
-			def.PairListMode,
-			pairContinuationToken,
-		},
-		Read: []def.Pair{
-			def.PairOffset,
-			def.PairIoCallback,
-			def.PairSize,
-		},
-		Write: []def.Pair{
-			def.PairContentMD5,
-			def.PairContentType,
-			def.PairOffset,
-			def.PairIoCallback,
-		},
-		Stat: []def.Pair{
-			def.PairObjectMode,
-		},
+		// Create: []def.Pair{
+		// 	def.PairObjectMode,
+		// },
+		// Delete: []def.Pair{
+		// 	def.PairObjectMode,
+		// },
+		// List: []def.Pair{
+		// 	def.PairListMode,
+		// 	def.PairContinuationToken,
+		// },
+		// Read: []def.Pair{
+		// 	def.PairOffset,
+		// 	def.PairIoCallback,
+		// 	def.PairSize,
+		// },
+		// Write: []def.Pair{
+		// 	def.PairContentMD5,
+		// 	def.PairContentType,
+		// 	def.PairOffset,
+		// 	def.PairIoCallback,
+		// },
+		// Stat: []def.Pair{
+		// 	def.PairObjectMode,
+		// },
 	},
-}
-
-var pairContinuationToken = def.Pair{
-	Name:        "continuation_token",
-	Type:        def.Type{Name: "string"},
-	Description: "specify the continuation token for list.",
 }
